c17a293dc3ab1c7fb81752f39de8185d3bc3d0a9
kevanultroneous <dev21.ultroneous@gmail.com>
2022-11-16 12:43:45 +0530

    post uploading width fixed

--- Client/src/components/PostUploading.js
+++ Client/src/components/PostUploading.js
@@ -615,7 +615,7 @@ export default function PostUploading() {
                 </Form.Label>
                 <InputGroup className="mb-3">
                   <InputGroup.Text id="basic-addon3">
-                    https://unmediabuzz.com/
+                    https://xyz.com/
                   </InputGroup.Text>
                   <Form.Control
                     id="basic-url"
