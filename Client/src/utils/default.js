dd726e130105c9bc31003e91f95ce42647eedd0f
kevanultroneous <dev21.ultroneous@gmail.com>
2022-12-13 16:31:53 +0530

    default js url chnages

--- Client/src/utils/default.js
+++ Client/src/utils/default.js
@@ -1,2 +1,3 @@
-export const defaultUrl = `http://192.168.1.28:8000/`;
+// export const defaultUrl = `http://192.168.1.28:8000/`;
+export const defaultUrl = `https://unmediabuzz.com:8000/`;
 export const frontendurl = `https://unmediabuzz.com/`;
